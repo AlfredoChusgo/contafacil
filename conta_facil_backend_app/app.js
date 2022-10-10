797d3931f71b60d55e43eecb7beca148357ec799
AlfredoCh <alfredochsoliz@gmail.com>
2022-10-09 21:17:31 -0400

    Update server cors problem using cors package

--- conta_facil_backend_app/app.js
+++ conta_facil_backend_app/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.delete('/user/:id', async (req, res) => {
