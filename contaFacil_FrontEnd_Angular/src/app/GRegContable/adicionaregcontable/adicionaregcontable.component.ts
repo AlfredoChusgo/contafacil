0142eeb1bf547a0d591c248c6fe8a07d829febbd
CORP\rsuarezl <Roheru>
2022-10-12 10:16:08 -0400

    adicionar registro contable (falta completar la vista)

--- contaFacil_FrontEnd_Angular/src/app/GRegContable/adicionaregcontable/adicionaregcontable.component.ts
+++ contaFacil_FrontEnd_Angular/src/app/GRegContable/adicionaregcontable/adicionaregcontable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RegContableservRegConta } from 'src/app/servRegConta/reg-contable.servRegConta';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionaregcontable',
@@ -6,10 +8,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adicionaregcontable.component.scss']
 })
 export class AdicionaregcontableComponent implements OnInit {
+  saveresp:any;
+  message='';
+  messageclass='';
+  editdata:any;
+  regContaID:any;
 
-  constructor() { }
+  constructor(private servRegConta:RegContableservRegConta) { }
 
-  ngOnInit(): void {
+
+  ngOnInit(): void { 
+  }
+
+  form = new FormGroup({
+    id: new FormControl({ value: '', disabled: true }),
+    date: new FormControl('', Validators.required),
+    total: new FormControl('', Validators.required),
+    recordType: new FormControl('', Validators.required),
+    userId: new FormControl('', Validators.required),
+    companyId: new FormControl('', Validators.required),
+    productId: new FormControl('', Validators.required)
+  });
+
+  guardarProducto() { 
+    if (this.form.valid) {
+      if(this.regContaID ==null){console.log('not null'+this.regContaID)
+        this.servRegConta.SaveRegistro(this.form.value).subscribe(
+          result => { 
+            if(result != null){    
+                this.saveresp = result;             
+                if (this.saveresp.message != 'added') {
+                  this.message = "Guardado Correctamente."
+                  this.messageclass = 'sucess'
+                  //this.limpiarform()
+  
+                } else if(this.saveresp.message == 'updated'){
+                  this.message = "Actualizado correctamente."
+                  this.messageclass = "sucess"
+                }else{
+                  this.message = "Falló al Guardar."
+                  this.messageclass = 'error'
+                }
+            }else{
+              this.message = "Falló al Guardar."
+              this.messageclass = 'error'
+            }  
+        });
+      }else{
+        this.servRegConta.UpdateRegistro(this.form.value).subscribe(
+          result => { 
+            if(result != null){    
+                this.saveresp = result;             
+                if (this.saveresp.message != 'added') {
+                  this.message = "Guardado Correctamente."
+                  this.messageclass = 'sucess'
+                  this.limpiarform()
+  
+                } else if(this.saveresp.message == 'updated'){
+                  this.message = "Actualizado correctamente."
+                  this.messageclass = "sucess"
+                }else{
+                  this.message = "Falló al Guardar."
+                  this.messageclass = 'error'
+                }
+            }else{
+              this.message = "Falló al Guardar."
+              this.messageclass = 'error'
+            }  
+        });
+      }
+    } else {
+      this.message = "Por favor ingresar datos válidos. Verifique..."
+      this.messageclass = 'error'
+    }
   }
 
 }
--- contaFacil_FrontEnd_Angular/src/app/service/reg-contable.service.ts
+++ contaFacil_FrontEnd_Angular/src/app/service/reg-contable.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import {  tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,24 @@ export class RegContableService {
 
   LoadAllRegContable():Observable<any>{
     return this.http.get('/api/accountingRecord');
+  }
+
+  SaveRegistro(data:any){
+    return this.http.post('/api/accountingRecord',data).pipe(
+      tap((data)=>{
+        this.refreshrequired.next()
+          console.log(data);
+      })
+    );
+  }
+
+  UpdateRegistro(data:any){
+    return this.http.post('/api/editaccountingRecord/',data).pipe(
+      tap((data)=>{
+        this.refreshrequired.next()
+          console.log(data);        
+      })
+    );
   }
 
 
