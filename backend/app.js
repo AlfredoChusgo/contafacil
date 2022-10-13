9b2755b5a9ae9e6a367f20ff53b5fb0613f7d26f
AlfredoCh <alfredochsoliz@gmail.com>
2022-10-13 12:45:13 -0400

    #100 Configure BACKEND Cors to allow request from local angular server

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,24 @@ import ExcelJS from 'exceljs'
 
 const prisma = new PrismaClient()
 const app = express()
-//const ExcelJS = require('exceljs/dist/es5');
 
-app.use(cors());
+var allowedOrigins = ['http://localhost:4200',
+                      'http://yourapp.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json())
 
 app.delete('/user/:id', async (req, res) => {
