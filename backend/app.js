cd18942e428c374b1f64619281ec67e34b40381b
AlfredoCh <alfredochsoliz@gmail.com>
2022-10-14 02:40:12 -0400

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+
+var allowedOrigins = ['http://localhost:4200',
+                      'https://frontend-angular-conta-facil.mybluemix.net'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
